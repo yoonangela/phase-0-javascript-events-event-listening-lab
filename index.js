019c6c50ed4a259d7f78de6336a3615985e01214
Yeji-Yoon <angelayoon0102@gmail.com>
2025-01-13 17:31:46 +0900

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+    const input  = document.getElementById('button');
+    input.addEventListener('click', function(){
+        alert('I was clicked!');
+
+    });
+   
 }
+
+
